248581f4f53af05eeeb268e1ae0d6336d9bf5259
Andrew Smith <andrewsmithweb@gmail.com>
2019-05-13 11:51:42 -0500

    Updated grid script to run first event selection

--- ubcc1pi/Analyzers/EventSelection.h
+++ ubcc1pi/Analyzers/EventSelection.h
@@ -86,6 +86,11 @@ class EventSelection : public art::EDAnalyzer
             int          m_subRun;                ///< The subrun number
             int          m_event;                 ///< The event number
             bool         m_isSignal;              ///< If the event is a true CC1Pi signal
+            
+            // Event level selection info
+            bool         m_didSelectionFinish;    ///< If the event selection finished (some events have PFParticles without tracks etc)
+            float        m_eventCC1PiScore;       ///< The event level CC1Pi score
+            int          m_nProtonSelected;       ///< The number of protons selected
         
             // Matched True Particle details
             bool         m_hasMatchedMCParticle;  ///< If the particle has a matched MCParticle
@@ -98,6 +103,49 @@ class EventSelection : public art::EDAnalyzer
             float        m_trackShowerScore;      ///< The track vs. shower score
             float        m_protonMIPScore;        ///< The proton vs. MIP score
             float        m_muonPionScore;         ///< The muon vs. pion score
+        
+            float        m_muonLikelihood;        ///< The score likelihood under the muon hypothesis
+            float        m_pionLikelihood;        ///< The score likelihood under the pion hypothesis
+            float        m_protonLikelihood;      ///< The score likelihood under the proton hypothesis
+            
+            bool         m_isMuonCandidate;       ///< Is this the most likely muon candidate
+            bool         m_isPionCandidate;       ///< Is this the most likely pion candidate
+            bool         m_isProtonCandidate;     ///< is this the most likely proton candidate
+        };
+
+        /**
+         *  @brief  The output event level structure
+         */
+        struct OutputEvent
+        {
+            // Event metadata
+            int          m_run;                     ///< The run number
+            int          m_subRun;                  ///< The subrun number
+            int          m_event;                   ///< The event number
+                                                    
+            // The truth interaction information    
+            bool         m_isSignal;                ///< If the event is a true CC1Pi signal
+            std::string  m_interaction;             ///< The interaction type string
+            bool         m_isNuFiducial;            ///< If the neutrino interaction is fiducial
+            float        m_nuE;                     ///< The true neutrino energy
+
+            // The true particle multiplicities
+            int          m_nMuMinus;                ///< The number of mu-
+            int          m_nMuPlus;                 ///< The number of mu+
+            int          m_nPiPlus;                 ///< The number of pi+
+            int          m_nPiMinus;                ///< The number of pi-
+            int          m_nKPlus;                  ///< The number of K+
+            int          m_nKMinus;                 ///< The number of K-
+            int          m_nProton;                 ///< The number of p
+            int          m_nNeutron;                ///< The number of n
+            int          m_nPhoton;                 ///< The number of gamma
+            int          m_nTotal;                  ///< The total number of particles
+
+            // The event selection info
+            bool         m_didSelectionFinish;      ///< If the event selection finished (some events have PFParticles without tracks etc)
+            float        m_cc1piScore;              ///< The event level CC1Pi score
+            int          m_nProtonSelected;         ///< The number of protons selected
+            int          m_nFinalStatePFParticles;  ///< The number of final state PFParticles
         };
 
         /**
@@ -119,7 +167,10 @@ class EventSelection : public art::EDAnalyzer
         art::EDAnalyzer::Table<Config>  m_config;          ///< The FHiCL configuration options
         
         TTree                          *m_pParticleTree;   ///< The output tree for all particle level data
+        TTree                          *m_pEventTree;      ///< The output tree for all event level data
+
         OutputParticle                  m_outputParticle;  ///< The output particle-level object
+        OutputEvent                     m_outputEvent;     ///< The output event-level object
 };
 
 } // namespace ubcc1pi
--- ubcc1pi/Analyzers/EventSelection_module.cc
+++ ubcc1pi/Analyzers/EventSelection_module.cc
@@ -19,22 +19,63 @@ EventSelection::EventSelection(const art::EDAnalyzer::Table<Config> &config) :
 {
     // Setup the output trees
     art::ServiceHandle<art::TFileService> fileService;
-    
+
+    // Particle tree
     m_pParticleTree = fileService->make<TTree>("particles", "");
     m_pParticleTree->Branch("run", &m_outputParticle.m_run);
     m_pParticleTree->Branch("subRun", &m_outputParticle.m_subRun);
     m_pParticleTree->Branch("event", &m_outputParticle.m_event);
     m_pParticleTree->Branch("isSignal", &m_outputParticle.m_isSignal);
-
-    m_pParticleTree->Branch("trackShowerScore", &m_outputParticle.m_trackShowerScore);
-    m_pParticleTree->Branch("protonMIPScore", &m_outputParticle.m_protonMIPScore);
-    m_pParticleTree->Branch("muonPionScore", &m_outputParticle.m_muonPionScore);
+    
+    m_pParticleTree->Branch("didSelectionFinish", &m_outputParticle.m_didSelectionFinish);
+    m_pParticleTree->Branch("eventCC1PiScore", &m_outputParticle.m_eventCC1PiScore);
+    m_pParticleTree->Branch("nProtonSelected", &m_outputParticle.m_nProtonSelected);
 
     m_pParticleTree->Branch("hasMatchedMCParticle", &m_outputParticle.m_hasMatchedMCParticle);
     m_pParticleTree->Branch("truePdgCode", &m_outputParticle.m_truePdgCode);
     m_pParticleTree->Branch("trueMomentum", &m_outputParticle.m_trueMomentum);
     m_pParticleTree->Branch("trueMatchPurity", &m_outputParticle.m_trueMatchPurity);
     m_pParticleTree->Branch("trueMatchCompleteness", &m_outputParticle.m_trueMatchCompleteness);
+
+    m_pParticleTree->Branch("trackShowerScore", &m_outputParticle.m_trackShowerScore);
+    m_pParticleTree->Branch("protonMIPScore", &m_outputParticle.m_protonMIPScore);
+    m_pParticleTree->Branch("muonPionScore", &m_outputParticle.m_muonPionScore);
+
+    m_pParticleTree->Branch("muonLikelihood", &m_outputParticle.m_muonLikelihood);
+    m_pParticleTree->Branch("pionLikelihood", &m_outputParticle.m_pionLikelihood);
+    m_pParticleTree->Branch("protonLikelihood", &m_outputParticle.m_protonLikelihood);
+    
+    m_pParticleTree->Branch("isMuonCandidate", &m_outputParticle.m_isMuonCandidate);
+    m_pParticleTree->Branch("isPionCandidate", &m_outputParticle.m_isPionCandidate);
+    m_pParticleTree->Branch("isProtonCandidate", &m_outputParticle.m_isProtonCandidate);
+
+    // Event tree
+    m_pEventTree = fileService->make<TTree>("events", "");
+
+    m_pEventTree->Branch("run", &m_outputEvent.m_run);
+    m_pEventTree->Branch("subRun", &m_outputEvent.m_subRun);
+    m_pEventTree->Branch("event", &m_outputEvent.m_event);
+
+    m_pEventTree->Branch("isSignal", &m_outputEvent.m_isSignal);
+    m_pEventTree->Branch("interaction", &m_outputEvent.m_interaction);
+    m_pEventTree->Branch("isNuFiducial", &m_outputEvent.m_isNuFiducial);
+    m_pEventTree->Branch("nuE", &m_outputEvent.m_nuE);
+
+    m_pEventTree->Branch("nMuMinus", &m_outputEvent.m_nMuMinus);
+    m_pEventTree->Branch("nMuPlus", &m_outputEvent.m_nMuPlus);
+    m_pEventTree->Branch("nPiPlus", &m_outputEvent.m_nPiPlus);
+    m_pEventTree->Branch("nPiMinus", &m_outputEvent.m_nPiMinus);
+    m_pEventTree->Branch("nKPlus", &m_outputEvent.m_nKPlus);
+    m_pEventTree->Branch("nKMinus", &m_outputEvent.m_nKMinus);
+    m_pEventTree->Branch("nProton", &m_outputEvent.m_nProton);
+    m_pEventTree->Branch("nNeutron", &m_outputEvent.m_nNeutron);
+    m_pEventTree->Branch("nPhoton", &m_outputEvent.m_nPhoton);
+    m_pEventTree->Branch("nTotal", &m_outputEvent.m_nTotal);
+
+    m_pEventTree->Branch("didSelectionFinish", &m_outputEvent.m_didSelectionFinish);
+    m_pEventTree->Branch("cc1piScore", &m_outputEvent.m_cc1piScore);
+    m_pEventTree->Branch("nProtonSelected", &m_outputEvent.m_nProtonSelected);
+    m_pEventTree->Branch("nFinalStatePFParticles", &m_outputEvent.m_nFinalStatePFParticles);
 }
 
 // -----------------------------------------------------------------------------------------------------------------------------------------
@@ -62,12 +103,42 @@ void EventSelection::analyze(const art::Event &event)
 
     // Make a CC1Pi event selector to do the legwork
     EventSelector selector(finalStates, event, pfParticleLabel, trackLabel, pidLabel);
+    
+    // Get the PIDs under the CC1Pi assumption
+    float cc1piScore = -std::numeric_limits<float>::max();
+    art::Ptr<recob::PFParticle> muonCandidate, pionCandidate;
+    PFParticleVector protonCandidates;
 
-    // Dump the output to the terminal
-    DebugHelper::Print(interaction);
-    std::cout << "Is true CC1Pi+? : " << isTrueSignal << std::endl;
+    if (finalStates.size() >= 2)
+        cc1piScore = selector.GetBestCC1PiScore(muonCandidate, pionCandidate, protonCandidates);
 
-    // Event level information
+    // Output event level information
+    m_outputEvent.m_run = event.run();
+    m_outputEvent.m_subRun = event.subRun();
+    m_outputEvent.m_event = event.event();
+    m_outputEvent.m_isSignal = isTrueSignal;
+    m_outputEvent.m_interaction = DebugHelper::GetInteractionString(interaction, true);
+    m_outputEvent.m_isNuFiducial = AnalysisHelper::IsNeutrinoVertexFiducial(interaction);
+    m_outputEvent.m_nuE = interaction.GetNeutrino().E();
+
+    const auto mcParticles = AnalysisHelper::GetReconstructableFinalStates(interaction);
+    m_outputEvent.m_nMuMinus = AnalysisHelper::CountParticlesWithPDG(mcParticles, 13);
+    m_outputEvent.m_nMuPlus = AnalysisHelper::CountParticlesWithPDG(mcParticles, -13);
+    m_outputEvent.m_nPiPlus = AnalysisHelper::CountParticlesWithPDG(mcParticles, 211);
+    m_outputEvent.m_nPiMinus = AnalysisHelper::CountParticlesWithPDG(mcParticles, -211);
+    m_outputEvent.m_nKPlus = AnalysisHelper::CountParticlesWithPDG(mcParticles, 321);
+    m_outputEvent.m_nKMinus = AnalysisHelper::CountParticlesWithPDG(mcParticles, -321);  
+    m_outputEvent.m_nProton = AnalysisHelper::CountParticlesWithPDG(mcParticles, 2212); 
+    m_outputEvent.m_nNeutron = AnalysisHelper::CountParticlesWithPDG(mcParticles, 2112);
+    m_outputEvent.m_nPhoton = AnalysisHelper::CountParticlesWithPDG(mcParticles, 22);
+    m_outputEvent.m_nTotal = mcParticles.size();
+
+    m_outputEvent.m_cc1piScore = cc1piScore;
+    m_outputEvent.m_didSelectionFinish = (cc1piScore >= 0.f);
+    m_outputEvent.m_nProtonSelected = protonCandidates.size();
+    m_outputEvent.m_nFinalStatePFParticles = finalStates.size();
+
+    // Output particles
     m_outputParticle.m_run = event.run();
     m_outputParticle.m_subRun = event.subRun();
     m_outputParticle.m_event = event.event();
@@ -75,19 +146,40 @@ void EventSelection::analyze(const art::Event &event)
 
     for (const auto &pfParticle : finalStates)
     {
-        const auto trackShowerScore = selector.GetTrackShowerScore(pfParticle);
-        const auto protonMIPScore = selector.GetProtonMIPScore(pfParticle);
-        const auto muonPionScore = selector.GetMuonPionScore(pfParticle);
+        // Event-level selection information
+        m_outputParticle.m_eventCC1PiScore = cc1piScore;
+        m_outputParticle.m_didSelectionFinish = (cc1piScore >= 0.f);
+        m_outputParticle.m_nProtonSelected = protonCandidates.size();
 
-        std::cout << "PFParticle " << pfParticle->Self() << std::endl;
-        std::cout << "  - Track vs. Shower score : " << trackShowerScore << std::endl;
-        std::cout << "  - Proton vs. MIP score   : " << protonMIPScore << std::endl;
-        std::cout << "  - Muon vs. Pion score    : " << muonPionScore << std::endl;
-       
-        m_outputParticle.m_trackShowerScore = trackShowerScore;
-        m_outputParticle.m_protonMIPScore = protonMIPScore;
-        m_outputParticle.m_muonPionScore = muonPionScore;
+        // Particle-level selection information
+        m_outputParticle.m_trackShowerScore = selector.GetTrackShowerScore(pfParticle);
+        m_outputParticle.m_protonMIPScore = selector.GetProtonMIPScore(pfParticle);
+        m_outputParticle.m_muonPionScore = selector.GetMuonPionScore(pfParticle);
 
+        m_outputParticle.m_muonLikelihood = selector.GetMuonLikelihood(pfParticle);
+        m_outputParticle.m_pionLikelihood = selector.GetPionLikelihood(pfParticle);
+        m_outputParticle.m_protonLikelihood = selector.GetProtonLikelihood(pfParticle);
+
+        m_outputParticle.m_isMuonCandidate = false;
+        m_outputParticle.m_isPionCandidate = false;
+        m_outputParticle.m_isProtonCandidate = false;
+
+        if (m_outputParticle.m_didSelectionFinish)
+        {
+            m_outputParticle.m_isMuonCandidate = (pfParticle == muonCandidate);
+            m_outputParticle.m_isPionCandidate = (pfParticle == pionCandidate);
+
+            for (const auto &protonCandidate : protonCandidates)
+            {
+                if (pfParticle != protonCandidate)
+                    continue;
+
+                m_outputParticle.m_isProtonCandidate = true;
+                break;
+            }
+        }
+
+        // MCParticle matching information
         m_outputParticle.m_hasMatchedMCParticle = false;
         m_outputParticle.m_truePdgCode = -std::numeric_limits<int>::max();
         m_outputParticle.m_trueMomentum = -std::numeric_limits<float>::max();
@@ -99,11 +191,6 @@ void EventSelection::analyze(const art::Event &event)
             const auto mcParticle = backtrackerData.GetBestMatchedMCParticle(pfParticle);
             const auto purity = backtrackerData.GetMatchPurity(pfParticle, mcParticle);
             const auto completeness = backtrackerData.GetMatchCompleteness(pfParticle, mcParticle);
-
-            std::cout << "  - True MCParticle " << mcParticle->TrackId() << std::endl;
-            std::cout << "    - PDG                  : " << mcParticle->PdgCode() << std::endl;
-            std::cout << "    - Purity               : " << purity << std::endl;
-            std::cout << "    - Completeness         : " << completeness << std::endl;
         
             m_outputParticle.m_hasMatchedMCParticle = true;
             m_outputParticle.m_truePdgCode = mcParticle->PdgCode();
@@ -117,6 +204,8 @@ void EventSelection::analyze(const art::Event &event)
 
         m_pParticleTree->Fill();
     }
+        
+    m_pEventTree->Fill();
 }
 
 } // namespace ubcc1pi
