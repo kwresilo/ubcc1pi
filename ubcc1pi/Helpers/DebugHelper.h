6bc063229df329c9a5ae051a0bb01f0a2b7dd8cd
Andrew Smith <andrewsmithweb@gmail.com>
2019-06-27 13:26:32 -0500

    Updated doxygen comments for debug helper - missing files

--- ubcc1pi/Helpers/DebugHelper.h
+++ ubcc1pi/Helpers/DebugHelper.h
@@ -32,19 +32,90 @@ class DebugHelper
          */
         static void Print(const art::Ptr<simb::MCParticle> &particle, const unsigned int depth = 0);
 
-        // TODO doxygen comments
+        /**
+         *  @brief  Print a vector of MCParticls 
+         *
+         *  @param  particles the particles to print
+         *  @param  depth the number of indents to use, for hierarchical information
+         */
         static void Print(const MCParticleVector &particles, const unsigned int depth = 0);
+
+        /**
+         *  @brief  Print a summary of a vector of MCParticles
+         *
+         *  @param  particles the particles to print
+         *  @param  depth the number of indents to use, for hierarchical information
+         */
         static void PrintSummary(const MCParticleVector &particles, const unsigned int depth = 0);
+
+        /**
+         *  @brief  Print an interaction
+         *
+         *  @param  interaction the interaction to print
+         *  @param  depth the number of indents to use, for hierarchical information
+         */
         static void Print(const TruthHelper::Interaction &interaction, const unsigned int depth = 0);
+
+        /**
+         *  @brief  Print a PFParticle
+         *
+         *  @param  particle the PFParticle to print
+         *  @param  depth the number of indents to use, for hierarchical information
+         */
         static void Print(const art::Ptr<recob::PFParticle> &particle, const unsigned int depth = 0);
+
+        /**
+         *  @brief  Print backtracker data
+         *
+         *  @param  data the backtracker data to print
+         *  @param  depth the number of indents to use, for hierarchical information
+         */
         static void Print(const BacktrackHelper::BacktrackerData &data, const unsigned int depth = 0);
+
+        /**
+         *  @brief  Get a string that describes a given interaction
+         *
+         *  @param  interaction the interaction from which to extract the string
+         *  @param  brief if we should use the brief name for the interaction
+         *
+         *  @return the interaction string
+         */
         static std::string GetInteractionString(const TruthHelper::Interaction &interaction, const bool brief = false);
+
+        /**
+         *  @brief  Print slice medatadata
+         *
+         *  @param  sliceMetadata the slice metadata to print
+         *  @param  depth the number of indents to use, for hierarchical information
+         */
         static void Print(const BacktrackHelper::SliceMetadata &sliceMetadata, const unsigned int depth = 0);
+
     private:
 
+        /**
+         *  @brief  Print a header line
+         *
+         *  @param  title the title of the header
+         *  @param  depth the number of indents to use, for hierarchical information
+         */
         static void PrintHeader(const std::string &title, const unsigned int depth = 0);
+
+        /**
+         *  @brief  Print a list item for a given property
+         *
+         *  @param  key the name of the property to print
+         *  @param  depth the number of indents to use, for hierarchical information
+         */
         static void PrintProperty(const std::string &key, const unsigned int depth = 0);
 
+        /**
+         *  @brief  Print a list item key-value pair for a given property
+         *
+         *  @tparam T the type of the property value to print
+         *  @param  key the name of the property to print
+         *  @param  value the value of the property to print
+         *  @param  depth the number of indents to use, for hierarchical information
+         */
         template <typename T>
         static void PrintProperty(const std::string &key, const T &value, const unsigned int depth = 0);
         
@@ -59,7 +130,6 @@ unsigned int DebugHelper::m_indent = 4;
 unsigned int DebugHelper::m_lineWidth = 90;
 unsigned int DebugHelper::m_keyWidth = 16;
 char DebugHelper::m_lineChar = '-';
-
 
 // -----------------------------------------------------------------------------------------------------------------------------------------
 
