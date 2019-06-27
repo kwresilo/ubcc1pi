bfdf7458dd66baf2463ee5336e7f48d5b4697f33
Andrew Smith <andrewsmithweb@gmail.com>
2019-06-27 14:01:48 -0500

    Filled doxygen comments in reco helper

--- ubcc1pi/Helpers/RecoHelper.h
+++ ubcc1pi/Helpers/RecoHelper.h
@@ -17,7 +17,7 @@ namespace ubcc1pi
 {
 
 /**
- *  @brief  A map intended to hold the mapping from PFParticle.TrackId() -> PFParticle - for navigation of the PFParticle hierarchy 
+ *  @brief  A map intended to hold the mapping from PFParticle.Self() -> PFParticle - for navigation of the PFParticle hierarchy 
  */
 typedef std::unordered_map<int, art::Ptr<recob::PFParticle> > PFParticleMap;
 
@@ -27,31 +27,30 @@ typedef std::unordered_map<int, art::Ptr<recob::PFParticle> > PFParticleMap;
 class RecoHelper
 {
     public:
-        // TODO fill out comments
         /**
-         *  @brief  
+         *  @brief  Get the mapping from PFParticle.Self() -> PFParticle
          *
-         *  @param  allPFParticles
+         *  @param  allPFParticles input vector of all PFParticles in the event
          *
-         *  @return 
+         *  @return the PFParticle map
          */
         static PFParticleMap GetPFParticleMap(const PFParticleVector &allPFParticles);
 
         /**
-         *  @brief  
+         *  @brief  Get all neutrino PFParticles in the input list
          *
-         *  @param  allPFParticles
+         *  @param  allPFParticles the input vector of PFParticles
          *
-         *  @return 
+         *  @return the neutrino PFParticles
          */
         static PFParticleVector GetNeutrinos(const PFParticleVector &allPFParticles);
 
         /**
-         *  @brief  
+         *  @brief  Get a single neutrino from the input list
          *
-         *  @param  allPFParticles
+         *  @param  allPFParticles the input vector of all PFParticles
          *
-         *  @return 
+         *  @return the neutrino PFParticle
          */
         static art::Ptr<recob::PFParticle> GetNeutrino(const PFParticleVector &allPFParticles);
 
@@ -59,151 +58,153 @@ class RecoHelper
          *  @brief  Get the PFParticles that have been deemed the neutrino final states from the PFParticle hierarchy
          *
          *  @param  allPFParticles the input vector of all PFParticles
+         *
+         *  @return the neutrino final state PFParticles
          */
         static PFParticleVector GetNeutrinoFinalStates(const PFParticleVector &allPFParticles);
 
         /**
-         *  @brief  
+         *  @brief  Get the reconstructed neutrino interaction vertex position
          *
-         *  @param  event
-         *  @param  allPFParticles
-         *  @param  vertexLabel
+         *  @param  event the art event record
+         *  @param  allPFParticles the input vector of all PFParticles
+         *  @param  vertexLabel the label for the vertex producer module
          *
-         *  @return 
+         *  @return the 3D position of the reconstructed interaction vertex
          */
         static TVector3 GetRecoNeutrinoVertex(const art::Event &event, const PFParticleVector &allPFParticles, const art::InputTag &vertexLabel);
 
         /**
-         *  @brief  
+         *  @brief  Get the parent of the input PFParticle
          *
-         *  @param  particle
-         *  @param  pfParticleMap
+         *  @param  particle the child PFParticle
+         *  @param  pfParticleMap the PFParticle mapping 
          *
-         *  @return 
+         *  @return the partent PFParticle
          */
         static art::Ptr<recob::PFParticle> GetParent(const art::Ptr<recob::PFParticle> &particle, const PFParticleMap &pfParticleMap);
 
         /**
-         *  @brief  
+         *  @brief  Get the daughters of the input PFParticle
          *
-         *  @param  particle
-         *  @param  pfParticleMap
+         *  @param  particle the parent PFParticle
+         *  @param  pfParticleMap the PFParticle mapping
          *
-         *  @return 
+         *  @return the daughter PFParticles
          */
         static PFParticleVector GetDaughters(const art::Ptr<recob::PFParticle> &particle, const PFParticleMap &pfParticleMap);
 
         /**
-         *  @brief  
+         *  @brief  Collect all particles downstream of the input PFParticle (including the particle itself)
          *
-         *  @param  particle
-         *  @param  pfParticleMap
+         *  @param  particle the input PFParticle
+         *  @param  pfParticleMap the PFParticle mapping
          *
-         *  @return 
+         *  @return the downstream PFParticles
          */
         static PFParticleVector GetDownstreamParticles(const art::Ptr<recob::PFParticle> &particle, const PFParticleMap &pfParticleMap);
 
         /**
-         *  @brief  
+         *  @brief  Collect all particles downstream of the input PFParticle (including the particle itself)
          *
-         *  @param  particle
-         *  @param  pfParticleMap
-         *  @param  downstreamParticles
+         *  @param  particle the input PFParticle
+         *  @param  pfParticleMap the PFParticle mapping
+         *  @param  downstreamParticles the output downstream PFParticles
          */
         static void GetDownstreamParticles(const art::Ptr<recob::PFParticle> &particle, const PFParticleMap &pfParticleMap, PFParticleVector &downstreamParticles);
 
         /**
-         *  @brief  
+         *  @brief  Determine if a given slice has been selected as a neutrino by looking at the PdgCodes of its PFParticles
          *
-         *  @param  slice
-         *  @param  slicesToPFParticles
+         *  @param  slice the slice to check
+         *  @param  slicesToPFParticles the mapping from slices to PFParticles
          *
-         *  @return 
+         *  @return if the slice is selected as a neutrino
          */
         static bool IsSliceSelectedAsNu(const art::Ptr<recob::Slice> &slice, const SlicesToPFParticles &slicesToPFParticles);
 
         /**
-         *  @brief  
+         *  @brief  Determine if a given property exists in the input metadata object
          *
-         *  @param  metadata
-         *  @param  property
+         *  @param  metadata the metadata object to check
+         *  @param  property the property value to look up
          *
-         *  @return 
+         *  @return if the proerty exists in the metadata
          */
         static bool HasMetadataValue(const art::Ptr<larpandoraobj::PFParticleMetadata> &metadata, const std::string &property);
 
         /**
-         *  @brief  
+         *  @brief  Get a metadata value with floating point type
          *
-         *  @param  metadata
-         *  @param  property
+         *  @param  metadata the metadata
+         *  @param  property the property name
          *
-         *  @return 
+         *  @return the value
          */
         static float GetMetadataFloat(const art::Ptr<larpandoraobj::PFParticleMetadata> &metadata, const std::string &property);
 
         /**
-         *  @brief  
+         *  @brief  Get a metadata value converted to integer type
          *
-         *  @param  metadata
-         *  @param  property
+         *  @param  metadata the metadata
+         *  @param  property the property name
          *
-         *  @return 
+         *  @return the value
          */
         static int GetMetadataInt(const art::Ptr<larpandoraobj::PFParticleMetadata> &metadata, const std::string &property);
 
         /**
-         *  @brief  
+         *  @brief  Get a metadata value converted to boolean type
          *
-         *  @param  metadata
-         *  @param  property
+         *  @param  metadata the metadata
+         *  @param  property the property name
          *
-         *  @return 
+         *  @return the value
          */
         static bool GetMetadataBool(const art::Ptr<larpandoraobj::PFParticleMetadata> &metadata, const std::string &property);
 
         /**
-         *  @brief  
+         *  @brief  Get the track vs. shower score assigned by pandora from a metadata object
          *
-         *  @param  metadata
+         *  @param  metadata the metadata
          *
-         *  @return 
+         *  @return the track score
          */
         static float GetTrackScore(const art::Ptr<larpandoraobj::PFParticleMetadata> &metadata);
 
         /**
-         *  @brief  
+         *  @brief  Get the neutrino vs. cosmic topological score from pandora from a metadata object
          *
-         *  @param  metadata
+         *  @param  metadata the metadata
          *
-         *  @return 
+         *  @return the neutrino vs. cosmic topological score
          */
         static float GetTopologicalScore(const art::Ptr<larpandoraobj::PFParticleMetadata> &metadata);
 
         /**
-         *  @brief  
+         *  @brief  Count the number of hits in a given view
          *
-         *  @param  hits
-         *  @param  view
+         *  @param  hits the full vector of hits
+         *  @param  view the view to use
          *
-         *  @return 
+         *  @return the number of hits
          */
         static unsigned int CountHitsInView(const HitVector &hits, const geo::View_t &view);
 
         /**
-         *  @brief  
+         *  @brief  Get the space charge service for later use
          *
-         *  @return 
+         *  @return the space charge service
          */
         static const spacecharge::SpaceChargeService::provider_type *const GetSpaceChargeService();
 
         /**
-         *  @brief  
+         *  @brief  Correct a given position for space charge (reco -> true)
          *
-         *  @param  position
-         *  @param  pSpaceChargeService
+         *  @param  position the input position
+         *  @param  pSpaceChargeService the space charge service
          *
-         *  @return 
+         *  @return the space charge corrected 3D position
          */
         static TVector3 CorrectForSpaceCharge(const TVector3 &position, const spacecharge::SpaceChargeService::provider_type *const pSpaceChargeService);
 };
