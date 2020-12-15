54e1bae917bc17a31122f7e82138754277fc1515
kirsty-duffy <ke.duffy1@gmail.com>
2020-12-15 13:12:58 -0600

    Also save flash time from flash-match anab::T0 to compare to the time of the largest flash

--- ubcc1pi/Helpers/RecoHelper.h
+++ ubcc1pi/Helpers/RecoHelper.h
@@ -96,6 +96,17 @@ class RecoHelper
         static float GetRecoFlashChi2(const art::Event &event, const PFParticleVector &allPFParticles, const art::InputTag &PFParticleLabel, const art::InputTag &flashmatchLabel);
 
         /**
+         *  @brief  Get the time from the flash-matching between the reconstructed neutrino and the measured flash
+         *
+         *  @param  event the art event record
+         *  @param  allPFParticles the input vector of all PFParticles
+         *  @param  flashmatchLabel the label for the flash matching producer module
+         *
+         *  @return the time from the flash-slice match
+         */
+        static float GetRecoFlashTime(const art::Event &event, const PFParticleVector &allPFParticles, const art::InputTag &PFParticleLabel, const art::InputTag &flashmatchLabel);
+
+        /**
          *  @brief  Get the largest flash in the event ( we will assume this is the one associated to the neutrino interaction if there is one)
          *
          *  @param  event the art event record
